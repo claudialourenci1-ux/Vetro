0fbb167b7c2c2bd527a189d191daa41dea6ba473
claudialourenci1-ux <claudialourenci1@gmail.com>
2026-08-28 16:51:28 -0300

    style: load commercial cockpit theme

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from 'next'
 import './globals.css'
 import './platform.css'
 import './workspace.css'
+import './cockpit.css'
 
 export const metadata: Metadata = {
   title: 'VETRO',
